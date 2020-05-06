001e3a0d2ead124b7f0872a87bee55d1450e143e
Lewis-Lucas <38751713+Lewis-Lucas@users.noreply.github.com>
2020-05-06 12:27:34 +0200

    Ajout commentaire

--- Bonjour.java
+++ Bonjour.java
@@ -1,3 +1,8 @@
+/**
+ *Fonction pour afficher la chaine "Hello world"
+ *Pre-condition : /
+ *Post-condition : string
+*/
 public class Bonjour {
     public static void main(String[] args) {
         System.out.println("Hello World");
